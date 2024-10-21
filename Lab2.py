2a4e15abefa88749bffd67b6c38aed24827ffaab
Chee Wei <cheewei21@gmail.com>
2024-10-21 10:16:33 +0800

    New calculator feature

--- Lab2.py
+++ Lab2.py
@@ -1,14 +1,38 @@
 import math
-def calculate_bmi (height, weight):
-    print ("Height: "+ str(height))
-    print ("Weight: "+ str(weight))
-    bmi= round(weight/height**2,2)
-    print(f"Your bmi is {bmi}")
-    if bmi<18.5:
-        print("You are underweight")
-    elif bmi>25:
-        print("You are over weight")
-    else:
-        print("You are normal weight")
 
-calculate_bmi(1.73,30) 
+def display_main_menu():
+    inp=input("Enter some numbers separated by commas (e.g. 5, 67, 32): ")
+    return inp
+def get_user_input():
+    inputs= display_main_menu()
+    string_numbers=inputs.split(',')
+    numbers=[]
+    for num in  string_numbers:
+        numbers.append(float(num))
+    return numbers
+
+
+def calc_average_temperature(numbers):
+      return  sum(numbers)/len(numbers)
+def calc_min_temperature(numbers):
+    return min(numbers)
+def calc_max_temperature(numbers):
+    return max(numbers)
+
+
+
+def display():
+    numbers = get_user_input()
+    average=round(calc_average_temperature(numbers),2)
+    maximum=calc_max_temperature(numbers)
+    minimum=calc_min_temperature(numbers)
+
+    print("Average temperature: "+str(average))
+    print("Maximum temperature: "+str(maximum))
+    print("Minimum temperature: "+str(minimum))
+
+
+display()
+
+if __name__=="__main__":
+    display()
