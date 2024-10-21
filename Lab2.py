9798a1bbece134ad3c9d8e36d9ce4f83b34c658c
Chee Wei <cheewei21@gmail.com>
2024-10-21 09:18:56 +0800

    Added bmi calculation feature

--- Lab2.py
+++ Lab2.py
@@ -1,1 +1,14 @@
-print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
+import math
+def calculate_bmi (height, weight):
+    print ("Height: "+ str(height))
+    print ("Weight: "+ str(weight))
+    bmi= round(weight/height**2,2)
+    print(f"Your bmi is {bmi}")
+    if bmi<18.5:
+        print("You are underweight")
+    elif bmi>25:
+        print("You are over weight")
+    else:
+        print("You are normal weight")
+
+calculate_bmi(1.73,30) 
