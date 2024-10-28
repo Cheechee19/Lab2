ac88cc0fa8a7a91c1806afd885674c0adb14dc68
Chee Wei <cheewei21@gmail.com>
2024-10-28 09:40:36 +0800

    Added return value

--- bmi.py
+++ bmi.py
@@ -6,14 +6,13 @@ def calculate_bmi (height, weight):
     print(f"Your bmi is {bmi}")
     if bmi<18.5:
         print("You are underweight")
-        x=-1
+        return -1
     elif bmi>25:
         print("You are over weight")
-        x=1
+        return 1
     else:
         print("You are normal weight")
-        x=0
-    return x
+        return 0
 
 calculate_bmi(1.73,30)
  
